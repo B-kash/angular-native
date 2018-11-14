d861d9860c8919be1efb9010900f985cdc553cf9
Bikash Chapagain <bikash.chapagain@yco.com.np>
2018-11-14 12:43:28 +0545

    float label added in add patient form

--- src/app/add-patient/add-patient.component.ts
+++ src/app/add-patient/add-patient.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {currentId} from "async_hooks";
 
 @Component({
   selector: 'app-add-patient',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddPatientComponent implements OnInit {
 
+  currentMonth;
+  currentYear;
+  currentDay;
+
   constructor() { }
 
   ngOnInit() {
+    let dates = new Date().toISOString().slice(10,0).split("-");
+    this.currentDay = dates[2];
+    this.currentMonth = dates[1];
+    this.currentYear = dates[0];
+
   }
 
 }
